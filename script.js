29dcf81c22ec654b84600d7a2eaf78361b183e53
ptasiek3003 <ptasiek3003@gmail.com>
2017-09-17 22:26:57 +0200

    switch cond

--- script.js
+++ script.js
@@ -11,11 +11,28 @@ $(document).ready(function() {
 	
 	var age_matur_total = 31556952; //czas w minutach okresu doroslego 70/min 18-60
 	var age_matur =age_matur_total -age_teen_total; //czas w minutach okresu doroslego 70/min 18-60
-	
+
 	var heart_bets_total;
+
+	var cond_wyczyn_fem = 57;
+	var cond_swietna_fem = 64;
+	var cond_dobra_fem = 67;
+	var cond_ponadprz_fem = 72;
+	var cond_przec_fem = 76;
+	var cond_slaba_fem = 80;
+	var cond_zla_fem = 84;
+
+	var cond_wyczyn_male = 53;
+	var cond_swietna_male = 60;
+	var cond_dobra_male = 65;
+	var cond_ponadprz_male = 69;
+	var cond_przec_male = 73;
+	var cond_slaba_male = 79;
+	var cond_zla_male = 83;
 	
 
 	$("#calculate").click(function(){
+
 		var birthday = document.getElementById("birth_date").value;
 		var birthday_hour = document.getElementById("birth_hour").value;
 		var minutes = parseInt(birthday_hour.substring(0,2))*60 + parseInt(birthday_hour.substring(3,6));
@@ -27,23 +44,13 @@ $(document).ready(function() {
 	if(!minutes){minutes =0;}
 
 	var diff = diff_minutes - minutes; //czas w minutach od urodzin po odjeciu godziny urodzenia
-	debugger;
+	
 	var sex =document.getElementById('sex').value;
-
-
+	var condition =document.getElementById('condition').value;
+debugger;
 		if(birthday){
 			if(diff>0){
-
-				switch(sex) {
-				    case "kobieta":
-				    	women();
-				        console.log("kobieta");
-				        break;
-				    case "mezczyzna":
-				    	male();
-				        console.log("mezczyzna");
-				        break;	        
-					}
+				heart_beats();
 				document.getElementById('content').innerHTML = (heart_bets_total + " uderzen serca");
 			}else{
 				document.getElementById('content').innerHTML = ("data urodzin nie moze byc pozniejsza od dzisiejszej");
@@ -52,7 +59,7 @@ $(document).ready(function() {
 			document.getElementById('content').innerHTML = ("prosze wpisac date urodzin");
 		}
 
-		function male() {
+		function heart_beats() {
 			if(diff_years<=3){
 				// calc_beats_child(130);
 				heart_bets_total= diff*130;
@@ -61,28 +68,40 @@ $(document).ready(function() {
 			}else if(diff_years<=18){
 				heart_bets_total = age_child*130+(age_kid )*100 + (diff- age_kid_total)*85;
 			}else if(diff_years<=60){
-			    heart_bets_total = age_child*130+(age_kid )*100 +(age_teen)*85 + (diff - age_teen_total)*70;
+				switch(condition) {
+				    case "dobra":
+				   		(sex == "kobieta") ? beats = count_bets_condition(cond_dobra_fem) : beats = count_bets_condition(cond_dobra_male);
+				        break;
+				     case "wyczynowa":
+				    	(sex == "kobieta") ? beats = count_bets_condition(cond_wyczyn_fem) : beats = count_bets_condition(cond_wyczyn_male);
+				        break;
+				    case "swietna":
+				    	(sex == "kobieta") ? beats = count_bets_condition(cond_swietna_fem) : beats = count_bets_condition(cond_swietna_male);
+				        break;
+				    case "ponadprzecietna":
+				    	(sex == "kobieta") ? beats = count_bets_condition(cond_ponadprz_fem) : beats = count_bets_condition(cond_ponadprz_male);
+				        break;
+				    case "przecietna":
+				    	(sex == "kobieta") ? beats = count_bets_condition(cond_przec_fem) : beats = count_bets_condition(cond_przec_male);
+				        break;
+				    case "slaba":
+				    	(sex == "kobieta") ? beats = count_bets_condition(cond_slaba_fem) : beats = count_bets_condition(cond_slaba_male);
+				        break;
+				    case "zla":
+				    	(sex == "kobieta") ?  beats =  count_bets_condition(cond_zla_fem) : beats =  count_bets_condition(cond_zla_male);
+				        break;
+				           
+					}
+
+			     heart_bets_total = age_child*130+(age_kid )*100 +(age_teen)*85 + beats;
 			}else{
 				heart_bets_total = age_child*130 + (age_kid )*100 + (age_teen)*85 + (age_matur)*70 +(diff-age_matur_total)*60;			
 			}
 		}
 
-		function women() {
-			if(diff_years<=3){
-				// calc_beats_child(130);
-				heart_bets_total= diff*135;
-			}else if(diff_years<=8){
-				heart_bets_total = age_child*135+(diff-age_child)*105;
-			}else if(diff_years<=18){
-				heart_bets_total = age_child*135+(age_kid )*105 + (diff- age_kid_total)*90;
-			}else if(diff_years<=60){
-			    heart_bets_total = age_child*135+(age_kid )*105 +(age_teen)*90 + (diff - age_teen_total)*80;
-			}else{
-				heart_bets_total = age_child*135 + (age_kid )*105 + (age_teen)*90 + (age_matur)*75 +(diff-age_matur_total)*60;			
-			}
+		function count_bets_condition(beats_count) {
+			return (diff - age_teen_total)*beats_count;
 		}
-
-
 	});
 });
 
