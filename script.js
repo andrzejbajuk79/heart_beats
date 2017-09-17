cb37ee43daf39277a9d9b6d5a98ef195aef8b716
ptasiek3003 <ptasiek3003@gmail.com>
2017-09-17 20:38:44 +0200

    switch

--- script.js
+++ script.js
@@ -27,28 +27,33 @@ $(document).ready(function() {
 	var minutes = parseInt(birthday_hour.substring(0,2))*60 + parseInt(birthday_hour.substring(3,6));
  	var now = moment(new Date(),"DD/MM/YYYY HH:mm"); //todays date
 	
-	var diff_years=now.diff(birthday,'years');
-	var diff2=now.diff(birthday,'minutes')  ;
+	var diff_years =now.diff(birthday,'years');
+	var diff_minutes = now.diff(birthday,'minutes')  ;
 	if(!minutes){
 		minutes =0;
 	}
 
-	var diff = diff2- minutes; //czas w minutach od urodzin po odjeciu godziny urodzenia
-debugger;
+	var diff = diff_minutes - minutes; //czas w minutach od urodzin po odjeciu godziny urodzenia
+	debugger;
+	var sex =document.getElementById('sex').value;
+
+
 	if(birthday){
 		if(diff>0){
+
+			switch(sex) {
+			    case "kobieta":
+			    	women();
+			        console.log("kobieta");
+			        break;
+			    case "mezczyzna":
+			    	male();
+			        console.log("mezczyzna");
+			        break;
+			  
+			        
+			}
 		
-		if(diff_years<=3){
-			heart_bets_per_min= diff*130;
-		}else if(diff_years<=8){
-			heart_bets_per_min = age_child*130+(diff-age_child)*100;
-		}else if(diff_years<=18){
-			heart_bets_per_min = age_child*130+(age_kid )*100 + (diff- age_kid_total)*85;
-		}else if(diff_years<=60){
-		    heart_bets_per_min = age_child*130+(age_teen)*100 + (diff - age_teen_total)*70;
-		}else{
-			heart_bets_per_min = age_child*130 + (age_teen)*100 + (age_matur)*70 +(diff-age_matur_total)*60;			
-		}
 			document.getElementById('content').innerHTML = (heart_bets_per_min + " uderzen serca");
 		}else{
 			document.getElementById('content').innerHTML = ("data urodzin nie moze byc pozniejsza od dzisiejszej");
@@ -56,8 +61,42 @@ debugger;
 	}else{
 		document.getElementById('content').innerHTML = ("prosze wpisac date urodzin");
 	}
-	
-	
+
+
+
+			function male() {
+				if(diff_years<=3){
+					// calc_beats_child(130);
+					heart_bets_per_min= diff*130;
+				}else if(diff_years<=8){
+					heart_bets_per_min = age_child*130+(diff-age_child)*100;
+				}else if(diff_years<=18){
+					heart_bets_per_min = age_child*130+(age_kid )*100 + (diff- age_kid_total)*85;
+				}else if(diff_years<=60){
+				    heart_bets_per_min = age_child*130+(age_kid )*100 +(age_teen)*85 + (diff - age_teen_total)*70;
+				}else{
+					heart_bets_per_min = age_child*130 + (age_kid )*100 + (age_teen)*85 + (age_matur)*70 +(diff-age_matur_total)*60;			
+				}
+
+			}
+
+			function women() {
+				if(diff_years<=3){
+					// calc_beats_child(130);
+					heart_bets_per_min= diff*135;
+				}else if(diff_years<=8){
+					heart_bets_per_min = age_child*135+(diff-age_child)*105;
+				}else if(diff_years<=18){
+					heart_bets_per_min = age_child*135+(age_kid )*105 + (diff- age_kid_total)*90;
+				}else if(diff_years<=60){
+				    heart_bets_per_min = age_child*135+(age_kid )*105 +(age_teen)*90 + (diff - age_teen_total)*80;
+				}else{
+					heart_bets_per_min = age_child*135 + (age_kid )*105 + (age_teen)*90 + (age_matur)*75 +(diff-age_matur_total)*60;			
+				}
+
+
+			}
+
 
 	});
 });
